07c8db6619f8bf18ad67d968ad0f94955c2c60ea
nickfury20baar-hub <nickfury20baar@gmail.com>
2026-04-20 20:34:44 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
-self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10761704
-}
-self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+self.addEventListener("install", (event) => {
+  console.log("Service Worker Installed");
+});
+
+self.addEventListener("fetch", (event) => {});
